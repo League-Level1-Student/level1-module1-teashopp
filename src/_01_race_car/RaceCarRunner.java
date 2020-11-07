36c7fc0332bdf0feac9d7818e32c8d543c06f065
ethan <ethan@192.168.86.34>
2020-11-06 16:52:21 -0800

    06/11

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -4,21 +4,19 @@ package _01_race_car;
  *    Level 1
  */
 
-
-
 public class RaceCarRunner {
 	public static void main(String[] args) {
 		/* Do the following things without changing the RaceCar class */
-		
+
 		// 1. Create a RaceCar and place it in 5th position
-	
+		RaceCar rc = new RaceCar("Ford", 5);
 		// 2. Print the RaceCar's position in the race
-
+		System.out.println(rc.getPositionInRace());
 		// 3. Crash the RaceCar
-		
+		rc.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+		rc.pit();
 		// 5. Help the car move into first place.
-
+		rc.overtake();
 	}
 }
